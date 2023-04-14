6fe07de3dc64c9739b0ab927b680daee7970a6f4
이국현 <lkukh17@naver.com>
2023-04-14 18:19:20 +0900

    fix

--- api/posting/apierror/ErrorResponse.go
+++ api/posting/apierror/ErrorResponse.go
@@ -1,21 +1,45 @@
 package apierror
 
 import (
-	"encoding/json"
-	"errors"
-	"gorm.io/gorm"
 	"net/http"
 )
 
-func HandleError(res http.ResponseWriter, errorResponse ErrorResponse) {
-	errorJson, _ := json.Marshal(errorResponse.ErrorBody)
-	res.WriteHeader(errorResponse.Status)
-	res.Write(errorJson)
+type ErrorBody struct {
+	Code    string
+	Message string
 }
 
-func BuildError(err error) ErrorResponse {
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		return ErrPostingNotExist()
+type ErrorResponse struct {
+	Status    int
+	ErrorBody ErrorBody
+}
+
+func ErrInternalServer() ErrorResponse {
+	return ErrorResponse{
+		Status: http.StatusInternalServerError,
+		ErrorBody: ErrorBody{
+			Code:    "X-500",
+			Message: "Internal Server Error",
+		},
 	}
-	return ErrInternalServer()
+}
+
+func ErrInvalidInput() ErrorResponse {
+	return ErrorResponse{
+		Status: http.StatusBadRequest,
+		ErrorBody: ErrorBody{
+			Code:    "X-400",
+			Message: "Input is Invalid",
+		},
+	}
+}
+
+func ErrPostingNotExist() ErrorResponse {
+	return ErrorResponse{
+		Status: http.StatusNotFound,
+		ErrorBody: ErrorBody{
+			Code:    "P-404",
+			Message: "Posting Not Exist",
+		},
+	}
 }
